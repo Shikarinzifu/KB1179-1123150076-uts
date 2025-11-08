803ea7d52dd60d388d8065667990dbf77173c23d
Ramzscuyyy <1123150076@global.ac.id>
2025-11-08 21:52:55 +0700

    mengubah images di splash 2

--- lib/screen/splash_screen2.dart
+++ lib/screen/splash_screen2.dart
@@ -1,10 +1,102 @@
 import 'package:flutter/material.dart';
 
+
 class Screen2 extends StatelessWidget {
   const Screen2({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Column(
+         mainAxisAlignment: MainAxisAlignment.center,
+         children: [
+          // Spacer(),
+          const SizedBox(height: 25),
+          Container(
+            height: 200,
+            width: 200,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: const Color.fromARGB(255, 104, 116, 104),
+              image: DecorationImage(image: AssetImage("assets/images/welcome2.png"),
+              fit: BoxFit.cover,
+              )
+            ),
+          ),
+          SizedBox(height: 20),
+          Text("Welcome",style: TextStyle(
+            fontSize: 30,
+            color: Colors.black
+          )),
+          Text("Don worry we got you cover \n Use wallie instead of cash",
+          textAlign: TextAlign.center,
+          style: TextStyle(  
+            fontSize: 12,
+            color: Colors.black
+          )),
+
+          SizedBox(height: 5),
+
+          Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            height: 10,
+                            width: 10,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.green,
+                            ),
+                          ),
+                          SizedBox(width: 5),
+                          Container(
+                            height: 10,
+                            width: 10,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Color(0xFFC8E6C9),
+                            ),
+                          ),
+                          SizedBox(width: 5),
+                          Container(
+                            height: 10,
+                            width: 10,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Color(0xFFC8E6C9),
+                            ),
+                          ),
+                        ],
+                      ),
+
+                      SizedBox(height: 40),
+                           Container(
+                            margin: EdgeInsets.all(40),
+                            child: SizedBox(
+                              height: 40,
+                              width: double.infinity,
+                            child: ElevatedButton(
+                            onPressed: () {
+                             
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.green,
+                            ),
+                             child: Text("Continue",style: TextStyle(
+                              color: Colors.white,
+                             )
+                             )
+                          ),
+                          )
+                          )
+
+
+
+
+         ], 
+        ),
+      ),
+    );
   }
 }
